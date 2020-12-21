d62c150f9a610bcfbfabdedd4eb99f445f014fd5
kush-x7 <kushagratripathi7@gmail.com>
2020-12-21 12:21:44 +0530

    h

---
+++ class.py
@@ -0,0 +1,10 @@
+class user :             --> declaring class
+    pass                 --> when you dont have anything to putt inside
+
+user_1=class()           -->creating object of class
+
+user_1.username ="kush"  -->.username is a "attribute" means variable
+
+class car:
+    def __init__(self):
+        print("whenerver a user will creaate new obj it will printed")
---
+++ libraries/1_turtle.py
@@ -0,0 +1,17 @@
+import turtle
+
+Turtle()                       -->class
+Screen()                       -->class
+
+obj_name =turtle.Turtle()       --->making obj of Turtle() class
+
+                                ---> a function inside a class is known as method
+obj_name.shape("turtle")        --->calling method shape
+obj.name.color("red")           --->for colloring turtle
+
+
+
+new_obj=Screen()
+
+new_obj.canvheight              --->for gui Screen
+new_obj.exitonclick()           --->when click then it will exit
